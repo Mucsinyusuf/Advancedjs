679226737c8a1c378dab1a3bfedac2a884529a44
mucsinyusuf <muxsinyussuf@gmail.com>
2024-05-30 05:02:12 -0400

    objects

--- objects.js
+++ objects.js
@@ -1,1 +1,23 @@
 // objects
+const car ={
+    name: 'Tesla',
+    model: 'S',
+    weight: 2250,
+    color: 'White'
+
+};// this is an object 
+
+// to acces properties in an object we use dot notation . . for example:
+console.log(car.color);
+console.log(car.model);
+console.log(car.name);
+console.log(car.weight);
+
+const myObject = {
+    name: 'Amazing Data type',
+    print: ()=> console.log("Hello, World!")
+
+
+}
+console.log(myObject.name)
+myObject.print()
